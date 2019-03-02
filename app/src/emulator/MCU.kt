b205beef5148d41294429cd7dc861df7c1bb20b4
Evgene <kinddarose@gmail.com>
2019-03-02 17:17:46 +0300

    Init MCU class

---
+++ app/src/emulator/MCU.kt
@@ -0,0 +1,13 @@
+package emulator
+
+import java.awt.Graphics2D
+import java.awt.Point
+
+class MCU(val name: String, val size: Double, val startPoint: Point) {
+
+  val pinNames = arrayListOf<String>()
+
+  fun drawTo(gr2: Graphics2D) {
+
+  }
+}
