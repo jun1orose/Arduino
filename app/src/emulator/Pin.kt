cb75ba7f1a73b1f1febc70ff587b8c0988451741
Evgene <kinddarose@gmail.com>
2019-03-03 12:04:46 +0300

    Add implementation for Pin

--- app/src/emulator/Pin.kt
+++ app/src/emulator/Pin.kt
@@ -1,8 +1,23 @@
 package emulator
 
+import java.awt.Color
+import java.awt.Graphics2D
 import java.awt.geom.Point2D
-import javax.swing.JComponent
 
-class Pin(private val pos: Point2D, private val size: Double): JComponent() {
-  
+class Pin(val pos: Point2D, val rad: Int) {
+
+  private var inputValue: Int? = null
+
+  fun drawTo(gr2: Graphics2D) {
+    gr2.color = Color.blue
+    gr2.drawOval(pos.x.toInt(), pos.y.toInt(), rad, rad)
+  }
+
+  fun setValue(value: Int) {
+    this.inputValue = value
+  }
+
+  fun isPin(point: Point2D): Boolean {
+    return Math.sqrt((point.x - pos.x) * (point.x - pos.x) + (point.y - pos.y) * (point.y - pos.y)) < rad
+  }
 }
