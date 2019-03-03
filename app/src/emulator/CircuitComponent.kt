a8c0bc8853679ed511b31d862b2145842c3cfe90
Evgene <kinddarose@gmail.com>
2019-03-03 14:44:10 +0300

    Add MCU and Pins for drawing

--- app/src/emulator/CircuitComponent.kt
+++ app/src/emulator/CircuitComponent.kt
@@ -2,17 +2,17 @@ package emulator
 
 import java.awt.*
 import java.awt.image.BufferedImage
-import java.io.File
-import javax.imageio.ImageIO
 import javax.swing.JComponent
 
 class CircuitComponent: JComponent() {
 
   private var buffer: BufferedImage? = null
   private var antiAlias = true
+  private object Pins { val INSTANCE = mutableListOf<Pin>()}
 
   override fun paintComponent(g: Graphics?) {
     super.paintComponent(g)
+    Pins.INSTANCE.clear()
 
     val needsNewBuffer = buffer == null
         || width != buffer?.width
@@ -22,16 +22,17 @@ class CircuitComponent: JComponent() {
 
       buffer = GraphicsEnvironment.getLocalGraphicsEnvironment().defaultScreenDevice.defaultConfiguration.createCompatibleImage(width, height)
 
-      val gr2 = buffer?.createGraphics()
+      val gr2 = buffer!!.createGraphics()
       enableAntiAlias(gr2)
-      gr2?.color = Color.WHITE
-      gr2?.fillRect(0, 0, width, height)
+      gr2.color = Color.WHITE
+      gr2.fillRect(0, 0, width, height)
 
+      val mcu = MCU("ATmega328", 400)
+      val startPoint = Point((width - mcu.width) / 2, (height - mcu.width) / 2)
+      mcu.drawTo(gr2, startPoint)
     }
 
     g?.drawImage(buffer, 0, 0, null)
-    val mcuImage: BufferedImage = ImageIO.read(File("app/src/emulator/avr.png"))
-    g?.drawImage(mcuImage, width / 2 - mcuImage.width / 2, height / 2 - mcuImage.height / 2, null)
 
   }
 
@@ -41,5 +42,13 @@ class CircuitComponent: JComponent() {
       gr2?.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
       gr2?.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY)
     }
+  }
+
+  companion object {
+    fun addPin(pin: Pin) {
+      Pins.INSTANCE.add(pin)
+    }
+
+    fun getPins() = Pins.INSTANCE
   }
 }
