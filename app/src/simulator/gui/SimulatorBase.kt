115009a79cd9290ff1def7bf679cc9f136f88dd2
Evgene <kinddarose@gmail.com>
2019-03-22 23:04:33 +0300

    Add uploadAndRun method

--- app/src/simulator/gui/SimulatorBase.kt
+++ app/src/simulator/gui/SimulatorBase.kt
@@ -1,5 +1,8 @@
 package simulator.gui
 
+import simulator.backend.PythonModule
+import java.io.File
+
 class SimulatorBase  {
 
   companion object {
@@ -7,12 +10,36 @@ class SimulatorBase  {
       Editor.createSimulator()
     }
 
-    fun uploadFirmwareToBoard(buildPath: String, sketchName: String): String? {
-      return null
+    fun uploadAndRun(buildPath: String, sketchName: String): String {
+
+      if (!Editor.isSimulatorExist()) {
+        return "Simulator not active!"
+      }
+
+      try {
+
+        val firmwareSrc = File("$buildPath/$sketchName.ino.elf")
+        if(!firmwareSrc.exists()) {
+          return "$sketchName.ino.elf doesn't exist!"
+        }
+
+        val firmwareDest = File("backend-sim-core/${firmwareSrc.name}")
+        if (firmwareDest.exists()) {
+          firmwareDest.delete()
+          firmwareSrc.copyTo(firmwareDest)
+        }
+
+        val mcu = CircuitComponent.getMCU().getCore()
+
+        PythonModule.initTable(mcu)
+        val status = PythonModule.uploadFirmwareAndRun(mcu, "$sketchName.ino.elf")
+
+        return status
+
+      } catch (e: Exception) {
+        e.printStackTrace()
+        return "Upload error!"
+      }
     }
   }
-
-
-
-
 }
