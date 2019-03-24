2bbfdabee28f2bc866d8f1e203b196b25f68e7ed
Evgene <kinddarose@gmail.com>
2019-03-24 20:36:58 +0300

    Test py module

--- backend-sim-core/sim.py
+++ backend-sim-core/sim.py
@@ -5,6 +5,7 @@ from pysimavr.connect import connect_pins_by_rule
 from pysimavr.firmware import Firmware
 from pysimavr.swig.simavr import avr_raise_irq
 from pysimavr.vcdfile import VcdFile
+import pysimavr.swig.utils as utils
 import sys
 
 
@@ -16,42 +17,49 @@ def create_avr(mcu, f_cpu, code):
 
 
 def test_avr():
-    mcu = 'atmega128'
+    mcu = 'atmega328'
     code = '''
     #include <avr/io.h>
     #include <util/delay.h>
 
     int main(void)
     {  
-        DDRA = 0xFF;
         DDRB = 0x00;
+        PORTB = 0xFF;
+        DDRC = 0x00;
+        PORTC = 0xFF;
         
         while(1)
         {
-            PORTA = PINB;
+            PORTB = PINB;
+            PORTC = PINC;
+            _delay_ms(1000);
         }
     }
     '''
 
-    def port_callback(irq, new_val):
-        print irq.name, new_val
-
-    callback_mock = Mock(side_effect=port_callback)
-
     avr = create_avr(mcu, 16000000, code)
+    table = parse_input_table(mcu)
 
     vcd = VcdFile(avr, period=1000, filename='test.vcd')
     connect_pins_by_rule('''
-                                        avr.A0 ==> vcd
-                                        avr.A1 ==> vcd
-                                        avr.A3 ==> vcd
-                                        avr.A4 ==> vcd
-                                        avr.A5 ==> vcd
-                                        avr.A6 ==> vcd
-                                        
-                                        avr.B0 ==> vcd
+                                        avr.B8 ==> vcd
                                         avr.B1 ==> vcd
                                         avr.B2 ==> vcd
+                                        avr.B3 ==> vcd
+                                        avr.B4 ==> vcd
+                                        avr.B5 ==> vcd
+                                        avr.B6 ==> vcd
+                                        avr.B7 ==> vcd
+                                        
+                                        avr.C0 ==> vcd
+                                        avr.C1 ==> vcd
+                                        avr.C2 ==> vcd
+                                        avr.C3 ==> vcd
+                                        avr.C4 ==> vcd
+                                        avr.C5 ==> vcd
+                                        avr.C6 ==> vcd
+                                        avr.C7 ==> vcd
                                         ''',
                          dict(
                              avr=avr,
@@ -60,19 +68,21 @@ def test_avr():
                          )
     vcd.start()
 
-    avr.irq.ioport_register_notify(callback_mock, ('A', 0))
-    avr.irq.ioport_register_notify(callback_mock, ('B', 0))
+    def port_callback(irq, new_val):
+        print irq.name, new_val
 
-    n = 100000000
+    callback_mock = Mock(side_effect=port_callback)
+
+    avr.irq.ioport_register_notify(callback_mock, ('B', 8))
+    avr.irq.ioport_register_notify(callback_mock, ('C', 8))
+
+    avr.step(20000000)
+    submit_values_to_pins(table, avr)
+
+    n = 90000000
     while n > 0:
-        avr_raise_irq(avr.irq.getioport(('B', 0)), 1)
-        avr_raise_irq(avr.irq.getioport(('B', 1)), 1)
-        avr_raise_irq(avr.irq.getioport(('B', 2)), 1)
         i = 10000000
         avr.step(i)
-        avr_raise_irq(avr.irq.getioport(('B', 0)), 0)
-        avr_raise_irq(avr.irq.getioport(('B', 1)), 0)
-        avr_raise_irq(avr.irq.getioport(('B', 2)), 0)
         n -= i
 
     avr.terminate()
@@ -80,12 +90,15 @@ def test_avr():
 
 def parse_input_table(file_name):
     with open(file_name) as f:
-        input_table = [tuple(i.split()) for i in f]
-
+        input_table = [tuple(line.split()) for line in f]
 
     return input_table
 
 
+def submit_values_to_pins(pins, avr):
+    for pin in pins:
+        avr_raise_irq(avr.irq.getioport((pin[0], int(pin[1]))), int(pin[2]))
+
+
 if __name__ == '__main__':
-    table = parse_input_table('atmega328')
     test_avr()
